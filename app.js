112a6d69461096610508306d63a7a58fc66b356e
thomas amado <lovenwar101@gmail.com>
2024-07-31 13:45:29 -0400

    pushing to the cloud

---
+++ app.js
@@ -0,0 +1,125 @@
+// italic function
+function italic() {
+    document.getElementById('writingArea').style.fontStyle = 'italic' ;
+}
+// italic event Listener
+// button id="btnItalic" onclick="italic()"
+document.getElementById('btnItalic').addEventListener('click', italic);
+
+
+
+
+// bold function
+function bold() {
+    document.getElementById('writingArea').style.fontWeight = 'bold' ;
+}
+// bold event Listener
+// button id="btnBold" onclick="bold()"
+document.getElementById('btnBold').addEventListener('click', bold);
+
+
+
+
+// left align function
+function leftAlign() {
+    document.getElementById('writingArea').style.textAlign = 'left' ;
+}
+// left align event Listener
+// button id="btnLeft" onclick="leftAlign()"
+document.getElementById('btnLeft').addEventListener('click', leftAlign);
+
+
+
+
+//center align function
+function centerAlign() {
+    document.getElementById('writingArea').style.textAlign = 'center' ;
+}
+// center align event Listener
+// button id="btnCenter" onclick="centerAlign()"
+document.getElementById('btnCenter').addEventListener('click', centerAlign);
+
+
+
+
+// right align function
+function rightAlign() {
+    document.getElementById('writingArea').style.textAlign = 'right' ;
+}
+// right align event Listener
+// button id="btnRight" onclick="rightAlign()"
+document.getElementById('btnRight').addEventListener('click', rightAlign);
+
+
+
+
+// uppercase function
+function uppercase() {
+    document.getElementById('writingArea').style.textTransform = 'uppercase' ;
+}
+// uppercase event Listener
+// button id="btnUppercase" onclick="uppercase()"
+document.getElementById('btnUppercase').addEventListener('click', uppercase);
+
+
+
+
+// Lowercase function
+function lowercase() {
+    document.getElementById('writingArea').style.textTransform = 'lowercase' ;
+}
+// Lowercase event Listener
+// button id="btnLowercase" onclick="lowercase()"
+document.getElementById('btnLowercase').addEventListener('click', lowercase);
+
+
+
+
+
+// capitalize function
+function capitalize() {
+    document.getElementById('writingArea').style.textTransform = 'capitalize' ;
+}
+// capitalize event Listener
+// button id="btnCapitalize" onclick="capitalize()"
+document.getElementById('btnCapitalize').addEventListener('click', capitalize);
+
+
+
+
+
+// clearvalue function
+function clearvalue() {
+    document.getElementById('writingArea').value = '' ;
+}
+// clearvalue event Listener
+// button id="btnClear" onclick="clearvalue()"
+document.getElementById('btnClear').addEventListener('click', clearvalue);
+
+
+//add red function
+function red() {
+    document.getElementById('writingArea').style.color = 'red' ;
+}
+//add red event Listener
+// button id="btnRed" onclick="red()"
+
+    document.getElementById('btnred').addEventListener('click', red);
+
+    //add blue function
+    function blue() {
+        document.getElementById('writingArea').style.color = 'blue' ;
+        }
+        
+        //add event listener
+        // button id="btnBlue" onclick="blue()"
+        document.getElementById('btnblue').addEventListener('click', blue);
+
+
+
+    
+    
+
+
+
+
